e1817c98ed73827b3c613c494e900cced38e625f
Ienxternal <125766428+Ienxternal@users.noreply.github.com>
2023-08-14 20:41:05 -0500

    Revert "adding leaderboard controller, routes, models, still need to test"

--- Routes/data/leaderboardRoutes.js
+++ Routes/data/leaderboardRoutes.js
@@ -8,21 +8,3 @@ router.get('/leaderboard', getGlobalLeaderboardController.getGlobalLeaderboard);
 router.get('/leaderboard/friends', getFriendLeaderboardController.getFriendLeaderboard);
 
 module.exports = router;
-
-
-
-//NEED TO FIX THESE
-
-// const express = require('express');
-// const router = express.Router();
-// const getGlobalLeaderboardController = require('../../controllers/getLeaderboard');
-// const getFriendLeaderboardController = require('../controllers/getFriendLeaderboard');
-
-// const Score = require('../../models/Score'); // Import the Score model
-// const Leaderboard = require('../../models/Leaderboard'); // Import the Leaderboard model
-
-// // Define routes for global leaderboard and friend leaderboard
-// router.get('/leaderboard', getGlobalLeaderboardController.getGlobalLeaderboard);
-// router.get('/leaderboard/friends', getFriendLeaderboardController.getFriendLeaderboard);
-
-// module.exports = router;
--- models/User.js
+++ models/User.js
@@ -21,14 +21,7 @@ const userSchema = new Schema({
         minlength: 8, // Increase the minimum length for enhanced security
         maxlength: 26,
     },
-
-    //ADDING FRIENDS
-    friends: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User', // Reference to other User documents
-    }],
 });
-
 
 userSchema.pre('save', async function (next) {
     try {
