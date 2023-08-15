1930f338dd3bb76bbed7e57151a419a0b785a5cf
userdefault13 <user.default@me.com>
2023-08-14 19:24:25 -0700

    both server and client is running on this commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,16 @@ import CreateGame from './pages/CreateGame';
 import Game from './pages/Game'; 
 
 const App = () => {
+    const handleSignup = async (username, email, password) => {
+        // Implement your signup logic here
+        // For example, you can make an API request to your backend
+    };
+
     return (
         <Router>
             <Routes>
                     <Route exact path="/" element={<Landing />} />
-                    <Route path="/Signup" element={<Signup />} />
+                    <Route path="/signup" element={<Signup handleSignup={handleSignup} />} />
                     <Route path="/Login" exact element={<Login />} />
                     <Route path="/Logout" element={<Logout />} />
                     <Route path="/Lobby" component={<Lobby />} />
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,10 +2,11 @@ import React from 'react';
 import SignupPage from '../components/SignupPage';
 
 function Signup({ handleSignup }) {
+    console.log('handleSignup function in Signup:', handleSignup); // Check if handleSignup is a function
     return (
         <div>
-        <h1>Welcome to Our App</h1>
-        <SignupPage handleSignup={handleSignup} />
+            <h1>Welcome to Our App</h1>
+            <SignupPage handleSignup={handleSignup} />
         </div>
     );
 }
