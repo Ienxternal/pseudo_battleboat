e54bdaa8f423764367792dac7d694dba6e8b7fca
rikilega <rosedoland33@yahoo.com>
2023-08-16 19:20:47 -0700

    modified loginpage so that form is displaying

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -38,17 +38,60 @@ const LoginPage = () => {
         await handleLogin();
     };
 
-    const Login = () => {
-        return (
-            <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-black black to-purple-900">
-                <div className='container mx-auto sm:px-6 lg:px-8 flex flex-col  px-8 py-8 rounded-full bg-black bg-opacity-10 shadow-md'>
-                    <h2 className="text-center mb-1 text-gray-500">Welcome to</h2>
-                    <h1 className="text-white text-center mb-4 text-5xl font-bold">BATTLEBOAT</h1>
+//     const Login = () => {
+//         return (
+//             <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-black black to-purple-900">
+//                 <div className='container mx-auto sm:px-6 lg:px-8 flex flex-col  px-8 py-8 rounded-full bg-black bg-opacity-10 shadow-md'>
+//                     <h2 className="text-center mb-1 text-gray-500">Welcome to</h2>
+//                     <h1 className="text-white text-center mb-4 text-5xl font-bold">BATTLEBOAT</h1>
                     
-                    <LoginPage />
-                </div>
+//                     <LoginPage />
+//                 </div>
+//             </div>
+//         );
+//     };
+// };
+
+return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-black black to-purple-900">
+    <div className='container mx-auto sm:px-6 lg:px-8 flex flex-col items-center px-8 py-8 rounded-full bg-black bg-opacity-10 shadow-md'>
+        <h2 className="text-center mb-1 text-gray-500">Welcome to</h2>
+        <h1 className="text-white text-center mb-4 text-5xl font-bold">BATTLEBOAT</h1>
+        
+        <form onSubmit={handleSubmit} className="w-full max-w-md mt-6">
+            <div className="mb-4">
+                <label className="block text-gray-500 text-sm font-bold mb-2" htmlFor="username">
+                    Username
+                </label>
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="username"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
             </div>
-        );
-    };
+            <div className="mb-6">
+                <label className="block text-gray-500 text-sm font-bold mb-2" htmlFor="password">
+                    Password
+                </label>
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div className="flex items-center justify-between">
+                <button type="submit" onClick={handleLogin} className="bg-purple-400 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    Login
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+);
 };
 export default LoginPage;
