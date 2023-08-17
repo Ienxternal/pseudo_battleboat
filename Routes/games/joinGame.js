7d7c16d6cd1650aa24241d0beead948c8985adc3
Ienxternal <alexanderttnguyen@gmail.com>
2023-08-16 19:36:39 -0500

    cleared

--- Routes/games/joinGame.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const joinGameController = require('../controllers/joinGame'); // Import the corresponding controller
-
-// Define the route for joining a game
-router.post('/:gameId/join', joinGameController.joinGame);
-
-module.exports = router;
